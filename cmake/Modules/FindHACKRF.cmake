8196ce4df88fb30778ea50a6180088d9d6816840
m4m00th <69227361+m4m00th@users.noreply.github.com>
2020-08-12 23:55:16 -0500

    Update FindHACKRF.cmake
    - comment  /usr/local/include/libhackrf
    - add path /usr/include/libhackrf
    - add path /usr/lib/x86_64-linux-gnu/

--- cmake/Modules/FindHACKRF.cmake
+++ cmake/Modules/FindHACKRF.cmake
@@ -10,8 +10,9 @@
 #MESSAGE("HACKRF_DIR set to ${HACKRF_DIR}" )
 
 FIND_PATH(HACKRF_INCLUDE_DIR hackrf.h
-  ${HACKRF_DIR}/include
-  /usr/local/include/libhackrf
+  ${HACKRF_DIR}
+#  /usr/local/include/libhackrf
+  /usr/include/libhackrf
 )
 
 FIND_LIBRARY(HACKRF_LIBRARY
@@ -22,6 +23,7 @@ FIND_LIBRARY(HACKRF_LIBRARY
   /usr/lib64
   /usr/lib
   /usr/local/lib
+  /usr/lib/x86_64-linux-gnu/
   NO_DEFAULT_PATH
 )
 
